62baae9c78d6a6b6bf8e616e5b122548185dbac6
cz <774666549@qq.com>
2017-11-13 18:27:55 +0800

    FileHelper文件新增文档注释

--- src/main/java/com/cz/xlsxReader/util/FileHelper.java
+++ src/main/java/com/cz/xlsxReader/util/FileHelper.java
@@ -3,7 +3,14 @@ package com.cz.xlsxReader.util;
 import java.io.File;
 import java.net.URL;
 
-
+/**
+ *<ol>
+ *<li>根据包名和文件名包装File对象</li>
+ *<li>根据给定的磁盘路径包装File对象 </li>
+ *</ol> 
+ * @author cz
+ *
+ */
 public class FileHelper {
 	//获取类路径文件
 	public static File fileInClassPath(String pkg,String name) {
